d3dc3ebca34ac0f3f3c3817047a62f5ca57f6729
Sergio Morales <destroyersemm@gmail.com>
2023-10-22 17:56:22 -0600

    Agregando cambios para el token jwt, actualizando log in

--- controllers/user.controller.js
+++ controllers/user.controller.js
@@ -1,52 +1,49 @@
+const jwt = require('jsonwebtoken');
 const User = require('../models/user.model.js').User;
+const { config } = require('../config/config');
 
-async function registrarUsuario(req, res){
+async function registrarUsuario(req, res) {
     const usern = req.body.us;
     const passw = req.body.pass;
-    
 
-    try{
-    const newUser = await new User({
-        username: usern,
-        password: passw
-    
+    try {
+        const newUser = await new User({
+            username: usern,
+            password: passw
         }).save();
         res.json({
             obj: newUser
-        })
+        });
     } catch (err) {
-    console.log(err);
+        console.log(err);
+        res.status(500).send('Error al registrar usuario');
     }
-    
 }
 
 async function login(req, res) {
     const usern = req.body.us;
     const passw = req.body.pass;
-    console.log(usern);
-    console.log(passw);
-  
+
     try {
-      const user = await User.findOne({ username: usern });
-      console.log(user);
-
-      if (user.password === passw) {
-        res.json({ 
-            message: 'login OK',
-            username: user.username,
-            password: user.password
-        });
-      }else{
-        res.status(401).json({message: 'login ERROR'});
-      }
-
+        const user = await User.findOne({ username: usern });
+        if (user && user.password === passw) {
+            // Generamos el JWT
+            const token = jwt.sign({ userId: user._id }, config.auth.secretKey, { expiresIn: '1h' });
+            res.json({
+                message: 'login OK',
+                token: token
+            });
+        } else {
+            res.status(401).json({ message: 'login ERROR' });
+        }
     } catch (err) {
-      console.log(err);
+        console.log(err);
+        res.status(500).send('Error al hacer login');
     }
-  }
-  
+}
+
 module.exports = {
     registrarUsuario,
     login
-}
+};
 
--- routes/auth.js
+++ routes/auth.js
@@ -1,14 +1,11 @@
 var express = require('express');
 var router = express.Router();
-
 const { firmarjwt, verifyjwt, servicioPrueba } = require('../controllers/auth.controller');
 
 /* GET home page. */
-router.post('/get-jwt', firmarjwt );
-router.post('/verify-jwt', verifyjwt);
-router.post('/test', verifyjwt, servicioPrueba)
-
-
+router.post('/get-jwt', firmarjwt);
+router.post('/verify-jwt', verifyjwt); 
+router.post('/test', verifyjwt, servicioPrueba); 
 
 module.exports = router;
 
