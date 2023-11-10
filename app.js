a4df66a3ebbfa997f975627f46aa3e7005b45a4b
Andres Legarreta <10548@ulsachihuahua.edu.mx>
2023-11-09 20:00:32 -0600

    agregando CORS

--- app.js
+++ app.js
@@ -3,6 +3,7 @@ var express = require('express');
 var path = require('path');
 var cookieParser = require('cookie-parser');
 var logger = require('morgan');
+var cors = require('cors')
 
 const mongoose = require('mongoose');
 
@@ -43,6 +44,8 @@ app.use('/ligas', ligasRouter);
 app.use('/partidos', partidosRouter);
 app.use('/noticias', noticiasRouter);
 app.use('/champ', champsRouter);
+
+app.use(cors());
 
 
 
